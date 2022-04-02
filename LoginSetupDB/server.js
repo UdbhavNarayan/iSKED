1beeffaf5ef04890b6910f7ec29ade54e4f3b081
Ekansh-Gahlot <60690453+Ekansh-Gahlot@users.noreply.github.com>
2022-04-02 19:59:34 +0530

    Add files via upload
    -> MongoDB link needs to be updated in server.js
    -> Employee api call name to be changed to user

---
+++ LoginSetupDB/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+// import socketio from "socket.io";
+// import WebSockets from "./utils/WebSockets";
+
+const EmployeeRoute = require('./routes/employee')
+const AuthRoute = require('./routes/auth')
+
+mongoose.connect('mongodb://localhost:27017/testdb', {useNewUrlParser: true, useUnifiedTopology: true} )
+const db = mongoose.connection
+
+db.on('error', (err) => {
+    console.log(err)
+})
+
+db.once('open', () => {
+    console.log('Database conncetion established')
+})
+
+
+const  app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use('/uploads', express.static('uploads'))
+
+const PORT = process.env.PORT || 3000
+// global.io - socketio.listen(PORT);
+// global.io.on('connection', WebSockets.connnection)
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
+app.use('/api/employee', EmployeeRoute)
+app.use('/api', AuthRoute)

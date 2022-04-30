d47b4191e737dee691e3761dd79060d29c8daedb
UdbhavNarayan <52520956+UdbhavNarayan@users.noreply.github.com>
2022-04-30 14:44:04 +0800

    Update AuthController.js

--- LoginSetupDB/controllers/AuthController.js
+++ LoginSetupDB/controllers/AuthController.js
@@ -1,6 +1,4 @@
 const User = require('../models/User')
-/*new line edited*/
-const UserVerification = require('../models/UserVerification')
 const bcrypt = require('bcryptjs')
 const jwt =  require('jsonwebtoken')
 const res = require('express/lib/response')

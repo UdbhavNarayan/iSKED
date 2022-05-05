201adba4353b372f2f414766374bb13c4ca16aaf
UdbhavNarayan <52520956+UdbhavNarayan@users.noreply.github.com>
2022-05-05 23:35:42 +0800

    Delete email-auth directory

--- email-auth/authenticate/authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegisterView, VerifyEmail
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name="register"),
-    path('email-verify/', VerifyEmail.as_view(), name="email-verify")
-]
